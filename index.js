7b30f112be83b0d35936d3d55a4796797a74d0a2
nuintun <nuintun@qq.com>
2017-11-10 18:02:39 +0800

    :hammer: rewrite

--- index.js
+++ index.js
@@ -1,59 +1,55 @@
-/*!
- * index
- *
- * Date: 2017/05/19
- * https://github.com/nuintun/gulp-util
- *
- * This is licensed under the MIT License (MIT).
- * For details, see: https://github.com/nuintun/gulp-util/blob/master/LICENSE
+/**
+ * @module index
+ * @license MIT
+ * @version 2017/11/10
  */
 
 'use strict';
 
-var Vinyl = require('vinyl');
-var chalk = require('chalk');
-var async = require('./lib/async');
-var Cache = require('./lib/cache');
-var enmu = require('./lib/enmu');
-var extend = require('./lib/extend');
-var hash = require('./lib/hash');
-var is = require('./lib/is');
-var plugins = require('./lib/plugins');
-var rename = require('./lib/rename');
-var transport = require('./lib/transport');
-var util = require('./lib/util');
+const Vinyl = require('vinyl');
+const chalk = require('chalk');
+const async = require('./lib/async');
+const Cache = require('./lib/cache');
+const enmu = require('./lib/enmu');
+const extend = require('./lib/extend');
+const hash = require('./lib/hash');
+const is = require('./lib/is');
+const plugins = require('./lib/plugins');
+const rename = require('./lib/rename');
+const transport = require('./lib/transport');
+const utils = require('./lib/utils');
 
 module.exports = {
-  Vinyl: Vinyl,
-  chalk: chalk,
-  async: async,
-  Cache: Cache,
-  CONCAT_STATUS: enmu.CONCAT_STATUS,
-  extend: extend,
-  hash: hash,
+  hash,
+  Vinyl,
+  chalk,
+  async,
+  Cache,
+  extend,
+  rename,
+  plugins,
+  transport,
   type: is.type,
   isString: is.isString,
   isPromise: is.isPromise,
   isFunction: is.isFunction,
   isPlainObject: is.isPlainObject,
-  plugins: plugins,
-  rename: rename,
-  transport: transport,
-  cwd: util.cwd,
-  slice: util.slice,
-  apply: util.apply,
-  debug: util.debug,
-  parseId: util.parseId,
-  isLocal: util.isLocal,
-  parseMap: util.parseMap,
-  normalize: util.normalize,
-  wrapVinyl: util.wrapVinyl,
-  blankVinyl: util.blankVinyl,
-  isRelative: util.isRelative,
-  isAbsolute: util.isAbsolute,
-  throwError: util.throwError,
-  isOutBound: util.isOutBound,
-  pathFromCwd: util.pathFromCwd,
-  inspectError: util.inspectError,
-  readonlyProperty: util.readonlyProperty
+  cwd: utils.cwd,
+  slice: utils.slice,
+  apply: utils.apply,
+  debug: utils.debug,
+  parseId: utils.parseId,
+  isLocal: utils.isLocal,
+  parseMap: utils.parseMap,
+  normalize: utils.normalize,
+  wrapVinyl: utils.wrapVinyl,
+  blankVinyl: utils.blankVinyl,
+  isRelative: utils.isRelative,
+  isAbsolute: utils.isAbsolute,
+  throwError: utils.throwError,
+  isOutBound: utils.isOutBound,
+  pathFromCwd: utils.pathFromCwd,
+  inspectError: utils.inspectError,
+  readonlyProperty: utils.readonlyProperty,
+  CONCAT_STATUS: enmu.CONCAT_STATUS
 };
