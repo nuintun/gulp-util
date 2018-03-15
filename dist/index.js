3082555948684ccab7e9f9a10c177d96ebd068fb
nuintun <nuintun@qq.com>
2018-03-15 11:52:32 +0800

    :art: Rewrite

--- dist/index.js
+++ dist/index.js
@@ -499,6 +499,59 @@ function log() {
 }
 
 /**
+ * @function info
+ */
+function info() {
+  const time = getTimestamp();
+
+  process.stdout.write(`${time} `);
+  apply(console.info, console, arguments);
+
+  return this;
+}
+
+/**
+ * @function dir
+ */
+function dir() {
+  const time = getTimestamp();
+
+  process.stdout.write(`${time} `);
+  apply(console.dir, console, arguments);
+
+  return this;
+}
+
+/**
+ * @function warn
+ */
+function warn() {
+  const time = getTimestamp();
+
+  process.stderr.write(`${time} `);
+  apply(console.warn, console, arguments);
+
+  return this;
+}
+
+/**
+ * @function error
+ */
+function error() {
+  const time = getTimestamp();
+
+  process.stderr.write(`${time} `);
+  apply(console.error, console, arguments);
+
+  return this;
+}
+
+log.info = info;
+log.dir = dir;
+log.warn = warn;
+log.error = error;
+
+/**
  * @module extend
  * @license MIT
  * @version 2017/11/10
