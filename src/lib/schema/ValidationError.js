130799e500d0b79aed3047705d3b144863b81faf
nuintun <nuintun@qq.com>
2018-12-27 11:59:43 +0800

    :art: Rewrite

--- src/lib/schema/ValidationError.js
+++ src/lib/schema/ValidationError.js
@@ -22,13 +22,21 @@ export default class ValidationError extends Error {
     this.message = `${name || ''} Invalid Options\n\n`;
 
     this.errors = errors.map(error => {
-      error.dataPath = error.dataPath.replace(/\//g, '.');
+      if (error.keyword === 'additionalProperties') {
+        error.dataPath = error.params.additionalProperty;
+      } else {
+        error.dataPath = error.dataPath.replace(/\//g, '.');
+      }
 
       return error;
     });
 
     this.errors.forEach(error => {
-      this.message += `options${error.dataPath} ${error.message}\n`;
+      if (error.keyword === 'additionalProperties') {
+        this.message += `unknown options: ${error.dataPath}\n`;
+      } else {
+        this.message += `options${error.dataPath} ${error.message}\n`;
+      }
     });
 
     Error.captureStackTrace(this, this.constructor);
